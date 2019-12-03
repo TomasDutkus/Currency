e725dfdcc53fc5dd755db09fe74be8dd31811ef9
Tomas Dutkus <tomas.dutkus@gmail.com>
2019-12-03 21:15:43 +0200

    Initial commit

--- src/lt/tomasdutkus/currency/Main.java
+++ src/lt/tomasdutkus/currency/Main.java
@@ -2,6 +2,12 @@ package lt.tomasdutkus.currency;
 
 import java.util.Scanner;
 
+/**
+ * An application to show currency exchange rates from bank of Lithuania
+ *
+ * @author Tomas
+ */
+
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
@@ -10,35 +16,40 @@ public class Main {
 
         UrlContent urlContent = new UrlContent();
 
+        // Printing menu
         printMenu();
 
         boolean quit = false;
         while (!quit) {
-            int action = scanner.nextInt();
+            String action = scanner.next();
             scanner.nextLine();
             switch (action) {
-                case 1:
+                case "1":
                     System.out.println("Please enter desired currency(LT/EU)");
                     String currency = scanner.nextLine();
                     System.out.println(urlContent.getURLContent("https://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=" + currency));
                     break;
-                case 2:
+                case "2":
                     System.out.println("Please enter your currency (LT/EU)");
                     String ourCurrency = scanner.nextLine();
                     System.out.println("Please enter foreign currency (USD)");
                     String foreignCurrency = scanner.nextLine();
-                    System.out.println("Please enter starting date (2019-02-02)");
+                    System.out.println("Please enter starting date (YYYY-MM-DD)");
                     String dateFrom = scanner.nextLine();
-                    System.out.println("Please enter ending date (2019-03-03)");
+                    System.out.println("Please enter ending date (YYYY-MM-DD)");
                     String dateTo = scanner.nextLine();
-                    System.out.println(urlContent.getURLContent("https://www.lb.lt/webservices/FxRates/FxRates.asmx/getFxRatesForCurrency?tp=" +ourCurrency+ "&ccy=" +foreignCurrency+ "&dtFrom=" +dateFrom+ "&dtTo=" +dateTo));
+                    System.out.println(urlContent.getURLContent("https://www.lb.lt/webservices/FxRates/FxRates.asmx/getFxRatesForCurrency?tp=" + ourCurrency + "&ccy=" + foreignCurrency + "&dtFrom=" + dateFrom + "&dtTo=" + dateTo));
                     break;
-                case 3:
+                case "3":
                     printMenu();
                     break;
-                case 4:
+                case "4":
                     System.out.println("Thank you for using this application");
                     quit = true;
+                    break;
+                default:
+                    System.out.println("Invalid input\n");
+                    printMenu();
                     break;
             }
         }
